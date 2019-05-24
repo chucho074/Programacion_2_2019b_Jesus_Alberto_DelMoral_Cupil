ca35c2b22c4f199e0aba2fdcb760153528f3d89c
chucho074 <idv18c.jmoral@uartesdigitales.edu.mx>
2019-05-24 11:56:10 -0500

    Final

--- Parcial 1/Practicas/zoologico/zoologico/Source.cpp
+++ Parcial 1/Practicas/zoologico/zoologico/Source.cpp
@@ -7,13 +7,13 @@ class aves : public vertebrados {};
 class reptiles : public vertebrados {};
 class anfibios : public vertebrados{};
 
-
 class carnivoros {};
 class herbivoros {};
 class omnivoros {};
 
 class vertebrados : public ANIMALES{};
 class invertebrados : public ANIMALES{};*/
+
 
 class ANIMALES {
 public:
@@ -24,9 +24,14 @@ public:
 	};
 
 	bool pelaje;
-
 	virtual void move() = 0;
+};
 
+class invertebrados : public ANIMALES {
+public:
+	virtual void move() = 0;
+	bool concha;
+	bool alas;
 };
 
 class vertebrados : public ANIMALES {
@@ -40,25 +45,77 @@ public:
 	bool pelaje;
 };
 
+class peces : public vertebrados {
+public: 
+	unsigned short num_aletas;
+	virtual void move() = 0;
+};
+
+class pulpo : invertebrados {
+public:
+	pulpo() {
+		concha = false;
+		alas = false;
+
+	}
+	void move(){ }
+};
 
 class lobo : public mamiferos {
 public:
 	lobo() {
 		pelaje = true;
-		bones = 3250;
+		bones = 320;
 	}
-	void move() {}
+	void move() { };
+};
+
+class leon : public mamiferos {
+public:
+	leon() {
+		pelaje = true;
+		bones = 525;
+	}
+	void move() { };
+};
+
+class tiburon : public peces {
+public:
+	tiburon() {
+		num_aletas = 6;
+	}
+	void move() { }
+};
+
+class medusa : public invertebrados {
+public:
+	medusa() {
+		concha = false;
+	}
+	void move() { };
+};
+
+class babosa : invertebrados {
+public:
+	babosa() {
+		concha = true;
+		alas = false;
+		}
+	void move() { }
 };
 
 
 
 
 
-
-
-
 int main() {
-	lobo puto;
-	
+	lobo Lobito;
+	pulpo Pulpito;
+	leon Leoncito;
+	tiburon Tiburoncin;
+	medusa Medusita;
+	babosa Babosita;
+
+		
 	return 0;
 }
