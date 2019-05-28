8f5d79a6c9e53c3768c7435c09004d778b08f849
chucho074 <idv18c.jmoral@uartesdigitales.edu.mx>
2019-05-27 23:42:42 -0500

    Avances
    No funciona, 8 errores que no se como corregir :(

--- Parcial 1/Practicas/zoologico/zoologico/Source.cpp
+++ Parcial 1/Practicas/zoologico/zoologico/Source.cpp
@@ -2,9 +2,13 @@
 #include <string>
 #include <vector>
 
+
+
+
 class ANIMALES {
 protected:
 	std::string name;
+	unsigned short age;
 public:
 	enum sexo {
 		Macho,
@@ -14,6 +18,17 @@ public:
 	bool pelaje;
 	virtual void move() = 0;
 	
+	void set_name(std::string name_in) {
+		name = name_in;
+	}
+
+	unsigned short return_age() {
+		return age;
+	}
+
+	void set_age(unsigned short age_in) {
+		age = age_in;
+	}
 };
 
 class invertebrados : public ANIMALES {
@@ -43,7 +58,6 @@ public:
 class pulpo : public invertebrados {
 public:
 	pulpo() {
-		name = "Pulpi";
 		concha = false;
 		alas = false;
 		pelaje = false;
@@ -55,7 +69,6 @@ public:
 class lobo : public mamiferos {
 public:
 	lobo() {
-		name = "Wolfi";
 		pelaje = true;
 		bones = 320;
 	}
@@ -65,7 +78,6 @@ public:
 class leon : public mamiferos {
 public:
 	leon() {
-		name = "Leonidas";
 		pelaje = true;
 		bones = 525;
 	}
@@ -84,7 +96,6 @@ public:
 class medusa : public invertebrados {
 public:
 	medusa() {
-		name = "Meducin";
 		concha = false;
 	}
 	void move() { };
@@ -93,24 +104,35 @@ public:
 class babosa : public invertebrados {
 public:
 	babosa() {
-		name = "Babocin";
 		concha = true;
 		alas = false;
 		}
 	void move() { }
 };
 
-
+void f_quicksort(unsigned short izq, std::vector<ANIMALES*> & vectorx, unsigned short der);
 
 
 
 int main() {
 	lobo Lobito;
+	Lobito.set_name("Wolfi");
+	Lobito.set_age(25);
 	pulpo Pulpito;
+	Pulpito.set_name("Pulpi");
+	Pulpito.set_age(5);
 	leon Leoncito;
+	Leoncito.set_name("Leonidas");
+	Leoncito.set_age(30);
 	tiburon Tiburoncin;
+	Tiburoncin.set_name("Tiburoncin");
+	Tiburoncin.set_age(15);
 	medusa Medusita;
+	Medusita.set_name("Meducin");
+	Medusita.set_age(3);
 	babosa Babosita;
+	Babosita.set_name("Babocin");
+	Babosita.set_age(1);
 
 	std::vector<ANIMALES*> vec_anim;
 	vec_anim.push_back(&Lobito);
@@ -119,8 +141,40 @@ int main() {
 	vec_anim.push_back(&Tiburoncin);
 	vec_anim.push_back(&Medusita);
 	vec_anim.push_back(&Babosita);
+	vec_anim[0]->return_age();
 
-
+	f_quicksort(0, vec_anim, vec_anim.size() - 1);
 
 	return 0;
+}
+
+
+void f_quicksort(unsigned short izq, std::vector<ANIMALES*> & vectorx, unsigned short der) {
+	unsigned short temp, i, j, piv;
+	i = izq;
+	j = der;
+	piv = (izq + der) / 2;
+
+	do {
+		while ((piv > vectorx[i]->return_age)) {
+			i++;
+		}
+		while ((piv < vectorx[j]->return_age)) {
+			j--;
+		}
+		if (i <= j) {
+			temp = vectorx[i]->return_age;
+			vectorx[i] = vectorx[j]->return_age;
+			vectorx[j]->return_age = temp;
+			i++;
+			j--;
+		}
+	} while (i <= j);
+
+	if (izq < j) {
+		f_quicksort(izq, vectorx, j);
+	}
+	if (der > i) {
+		f_quicksort(i, vectorx, der);
+	}
 }
