6cba0bc3663e1fb7c89cd5be565bd0295ff85918
chucho074 <idv18c.jmoral@uartesdigitales.edu.mx>
2019-07-22 13:40:11 -0500

    Update

--- Parcial 3/Practicas/Arboles/Arboles/Source.cpp
+++ Parcial 3/Practicas/Arboles/Arboles/Source.cpp
@@ -8,9 +8,45 @@ int main() {
 	arbolito->der->izq = new User("Majo", "Tapia",4);
 	arbolito->der->der = new User("Papo", "Tapia", 3);
 
-	arbolito->inO();
-	delete arbolito;
 
+	bool menu = true;
+	
+	while (menu) {
+		char opcion;
+		std::cin.clear();
+		
+		std::cout << "\n\nOpciones \n>in orden = 'i' \n>pre orden = 'p' \n>post orden = 'o'" << std::endl;
+		std::cin>>opcion;
+		switch (opcion) {
+			case 'i': {
+				arbolito->inO();
+				break;
+			}
+
+			case 'p': {
+				arbolito->preO();
+				break;
+			}
+
+			case 'o': {
+				arbolito->postO();
+				break;
+			}
+			
+			case 's': {
+				std::cout << "Bye" << std::endl;
+				menu = false;
+				break;
+			}
+			default: {
+				std::cout << "Error" << std::endl;
+				break;
+			}
+		}
+		
+	}
+
+	delete arbolito;
 	std::cin.clear();
 	std::cin.get();
 	return 0;
